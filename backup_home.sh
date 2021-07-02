910d7e362f5ba6723cc4ead1495d710aeb6e5e70
Benibr <1274814+benibr@users.noreply.github.com>
2021-07-02 20:02:28 +0200

    using config files

--- backup_home.sh
+++ backup_home.sh
@@ -2,20 +2,29 @@
 
 USER="$(id -un)"
 
-ping blackmesaeast.local -w1 -t1 &>/dev/null 
-if [ $? -ne 0 ]; then 
-  echo "cannot reach blackmesaeast.local" && exit 1
-fi
+source /home/$USER/.config/backup/environment
 
-trap 'sudo umount /mnt; exit 2' KILL TERM INT
+if [[ -n "${PING_TEST}" ]]; then
+  ping ${PING_TEST} -w1 -t1 &>/dev/null 
+  if [ $? -ne 0 ]; then 
+    echo "cannot reach ${PING_TEST}" && exit 1
+  fi
+fi
 
 echo "sudo required. Enter password if nessesary"
 sudo true && echo "sudo accuired" || exit 1
 
-sudo mount.cifs //blackmesaeast.local/backup /mnt/ -o user="$USER" || exit 1
-echo -n "restic password:"
-read -s pw
-declare -x RESTIC_PASSWORD="$pw"
-sudo -E restic backup / -r /mnt/$(id -un)/$(hostname)/restic/ -x --cleanup-cache --exclude "*Media*"
-sudo -E restic snapshots -r /mnt/$(id -un)/$(hostname)/restic/
+if [[ -n "${MOUNT_SMB}" ]]; then
+  trap 'sudo umount /mnt; exit 2' KILL TERM INT
+  sudo mount.cifs ${MOUNT_SMB} /mnt/ -o user="${USER}" || exit 1
+  [[ -z "${RESTIC_PASSWORD}" ]] && \
+    echo -n "restic password:" && \
+    read -s pw && \
+    declare -x RESTIC_PASSWORD="$pw"
+    declare -x RESTIC_REPOSITORY="/mnt/${USER}/$(hostname)/restic/"
+fi
+
+sudo -E restic backup ${FILESYSTEMS?} -x --cleanup-cache --exclude-file /home/${USER}/.config/backup/excludes
+sudo -E restic snapshots
+
 sudo umount /mnt
